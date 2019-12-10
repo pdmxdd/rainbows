de6f6c894987d5fe605f0809beeba8ea1bef75c8
paul <pdmxdd@gmail.com>
2019-12-10 00:11:57 -0600

    added rainbow-table & login POST

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import json
 from application_configuration.app import app, db
 from flask import request
-from util.util import make_json_response, hash_password
+from util.util import make_json_response, hash_password, check_password
 from models.user import User
 from scripts.seed_db import seed_db_with_users
+
+
+@app.route("/login", methods=["POST"])
+def post_login():
+    user = User.query.filter_by(email=request.json["email"]).first()
+
+    if not check_password(request.json["password"], user.password):
+        return make_json_response(json.dumps({"status": "error", "body": "incorrect user info"}), 400)
+
+    return make_json_response(json.dumps({"status": "success", "body": "Authenticated. Logging in."}), 200)
 
 
 @app.route("/register", methods=["POST"])
@@ -14,15 +24,22 @@ def post_register():
     if "email" not in request.json.keys():
         return make_json_response(json.dumps({"status": "error", "body": "bad request"}), 400)
 
+    if "password" not in request.json.keys():
+        return make_json_response(json.dumps({"status": "error", "body": "bad request"}), 400)
+
     if User.query.filter_by(email=request.json["email"]).first() is not None:
         return make_json_response(json.dumps({
             "status": "error",
             "body": "A user with that username already exists!"
         }), 400)
 
-    user = User(email=request.json["email"],
-                password=hash_password(request.json["password"]),
-                starting_balance=request.json["starting_balance"])
+    if "starting_balance" not in request.json.keys():
+        user = User(email=request.json["email"],
+                    password=request.json["password"])
+    else:
+        user = User(email=request.json["email"],
+                    password=request.json["password"],
+                    starting_balance=request.json["starting_balance"])
 
     db.session.add(user)
     db.session.commit()
@@ -39,11 +56,43 @@ def seed_db():
 @app.route("/breach-database", methods=["POST", "GET"])
 def breach_database():
     users = User.query.all()
-    print([user.to_dict() for user in users])
 
     return make_json_response(json.dumps({
         "users": [user.to_dict() for user in users]
     }), 200)
+
+
+@app.route("/rainbow", methods=["GET"])
+def get_rainbow_table():
+    passwords = {}
+    with open("bad-passwords.txt", 'r') as f:
+        temp = f.read()
+    for line in temp.split("\n"):
+        if hash_password(line) not in list(passwords.keys()):
+            passwords[hash_password(line)] = line
+    return make_json_response(json.dumps(passwords), 200)
+
+
+@app.route("/rainbow-table", methods=["GET"])
+def get_scan_rainbow_table():
+    passwords = {}
+
+    with open("bad-passwords.txt", 'r') as f:
+        temp = f.read()
+
+    for line in temp.split("\n"):
+        if hash_password(line) not in list(passwords.keys()):
+            passwords[hash_password(line)] = line
+
+    users = User.query.all()
+
+    hits = {}
+
+    for user in users:
+        if user.password in list(passwords.keys()):
+            hits[user.email] = passwords[user.password]
+
+    return make_json_response(json.dumps(hits), 200)
 
 
 if __name__ == "__main__":
